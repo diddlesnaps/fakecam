cd1eeaeb93b0828b9e4bc26be9a44c3b27dac297
Daniel Llewellyn <diddledan@ubuntu.com>
2020-05-27 20:52:35 +0100

    Bump version number to 2.0.0
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- fakecam/setup.py
+++ fakecam/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fakecam',
-    version='1.0.0',
+    version='2.0.0',
     url='https://elder.dev/posts/open-source-virtual-background/',
     author='Daniel Llewellyn',
     author_email='diddledan@ubuntu.com',
