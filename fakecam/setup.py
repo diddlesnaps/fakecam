22bf7c20cb1d50bfddcf695339c3a47876d01b94
Daniel Llewellyn <diddledan@ubuntu.com>
2021-02-18 17:56:05 +0000

    Minor update to 2.2.1
    * Ensure amdgpu.ids can be found
    * Update to lzo compression
    
    Signed-off-by: Daniel Llewellyn <diddledan@ubuntu.com>

--- fakecam/setup.py
+++ fakecam/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fakecam',
-    version='2.0.0',
+    version='2.2.1',
     url='https://elder.dev/posts/open-source-virtual-background/',
     author='Daniel Llewellyn',
     author_email='diddledan@ubuntu.com',
